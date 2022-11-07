b45f0b51a9aeceed3d05497830e8ca3607ecf133
Vitaliy Vynnyk <vitaliy.vinnik@gmail.com>
2022-11-07 23:07:23 +0100

    first commit

---
+++ lesson_1/task_1/index.js
@@ -0,0 +1,10 @@
+/* eslint-disable no-undef */
+
+// put your code here
+const rootElement = document.querySelector('#root');
+
+const greetingElem = React.createElement(
+  'div',
+  {className: 'greeting'},
+  'Hello, React!'
+)
