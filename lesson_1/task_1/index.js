7bedadb5f970d6028f78a535801100095b10e381
Vitaliy Vynnyk <vitaliy.vinnik@gmail.com>
2022-11-08 12:48:19 +0100

    lesson 1 task 3

--- lesson_1/task_1/index.js
+++ lesson_1/task_1/index.js
@@ -1,15 +1,22 @@
 /* eslint-disable no-undef */
+import React from 'react';
+import ReactDOM from 'react-dom';
 
-// put your code here
 const rootElement = document.querySelector('#root');
 
-const greetingElem = React.createElement(
-  'div',
-  {className: 'greeting'},
-  [
-    React.createElement('h1', {className: 'greeting__title'}, 'Hello, world!'),
-    React.createElement('p', {className: 'greeting__text'}, "I'm learning React"),
-  ]
-)
+
+const greetingElem = (<div className="greeting">
+  <h1 className="greeting__title">Hello, world!</h1>
+  <p className="greeting__text">I'm learning React</p>
+</div>);
+
+// const greetingElem = React.createElement(
+//   'div',
+//   {className: 'greeting'},
+//   [
+//     React.createElement('h1', {className: 'greeting__title'}, 'Hello, world!'),
+//     React.createElement('p', {className: 'greeting__text'}, "I'm learning React"),
+//   ]
+// )
 
 ReactDOM.render(greetingElem, rootElement);
